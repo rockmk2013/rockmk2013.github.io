c70c43b95c0715ab863e6d76f64a652d5d664e48
郭泰竹 <102306037@nccu.edu.tw>
2017-02-13 16:07:27 +0800

    lots of change

--- web.js
+++ web.js
@@ -1,53 +1,32 @@
 $(document).ready(function(){
-	$(window).scroll(function(){
-                 if($(window).scrollTop()>=$(".Head").offset().top && $(window).scrollTop()<$(".Aboutme").offset().top){
-                    $(".nav li").css("background-color","white")//除了被點擊到的游標，其他都恢復成原來的顏色
-                    $(".nav li:eq(0)").css("background-color","gray")
-                }else if($(window).scrollTop()>=$(".Aboutme").offset().top && $(window).scrollTop()<$(".Learn").offset().top){
-                    $(".nav li").css("background-color","white")//除了被點擊到的游標，其他都恢復成原來的顏色
-                    $(".nav li:eq(1)").css("background-color","gray")
-                }else if($(window).scrollTop()>=$(".Learn").offset().top && $(window).scrollTop()<$(".Music").offset().top){
-                    $(".nav li").css("background-color","white")//除了被點擊到的游標，其他都恢復成原來的顏色
-                    $(".nav li:eq(2)").css("background-color","gray")
-                }else if($(window).scrollTop()>=$(".Music").offset().top && $(window).scrollTop()<$(".Life").offset().top){
-                    $(".nav li").css("background-color","white")//除了被點擊到的游標，其他都恢復成原來的顏色
-                    $(".nav li:eq(3)").css("background-color","gray")
-                }else if($(window).scrollTop()>=$(".Life").offset().top ){
-                    $(".nav li").css("background-color","white")//除了被點擊到的游標，其他都恢復成原來的顏色
-                    $(".nav li:eq(4)").css("background-color","gray")
-                }
-            })
+	
 
-	$(".nav li:eq(0)").click(function(){
+	/*$(".nav li:eq(0)").click(function(){
 		$("html,body").stop()
 		$(".nav li").css("background-color","white")
-		$("html,body").animate({"scrollTop":$(".Head").offset().top-3})
+		$("html,body").animate({"scrollTop":$(".Head").offset().top-10})
 		$(".nav li:eq(0)").css("background-color","gray")
-	})
+	})*/
 	$(".nav li:eq(1)").click(function(){
 		$("html,body").stop()
-		$(".nav li").css("background-color","white")
 		$("html,body").animate({"scrollTop":$(".Aboutme").offset().top-8})
-		$(".nav li:eq(1)").css("background-color","gray")
 	})
 	$(".nav li:eq(2)").click(function(){
 		$("html,body").stop()
-		$(".nav li").css("background-color","white")
 		$("html,body").animate({"scrollTop":$(".Learn").offset().top+10})
-		$(".nav li:eq(2)").css("background-color","gray")
 	})
 	$(".nav li:eq(3)").click(function(){
 		$("html,body").stop()
-		$(".nav li").css("background-color","white")
 		$("html,body").animate({"scrollTop":$(".Music").offset().top+10})
-		$(".nav li:eq(3)").css("background-color","gray")
 	})
 	$(".nav li:eq(4)").click(function(){
 		$("html,body").stop()
-		$(".nav li").css("background-color","white")
 		$("html,body").animate({"scrollTop":$(".Life").offset().top+10})
-		$(".nav li:eq(4)").css("background-color","gray")
 	})
+  $(".nav li:eq(5)").click(function(){
+    $("html,body").stop()
+    $("html,body").animate({"scrollTop":$(".Contact").offset().top+10})
+  })
 	
   //計算導覽列置中的位置
     center()
